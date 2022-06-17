9466237f74067e32878b95ff59282272874080a4
Vikram Kumar <vikram@Vikrams-MacBook-Pro-2.local>
2022-06-17 16:07:32 -0600

    Added programatic navigate to order page

--- src/components/Order.tsx
+++ src/components/Order.tsx
@@ -1,8 +1,10 @@
 import { Button, Result } from 'antd';
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Order: React.FC = () => {
   const [orderNumber, setOrderNumber] = useState<number>(0);
+  const navigate = useNavigate();
 
   const generateOrderNumber = (max: number, min: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,13 +15,17 @@ const Order: React.FC = () => {
     setOrderNumber(order);
   }, []);
 
+  const navigatePage = () => {
+    navigate('/');
+  };
+
   return (
     <Result
       status='success'
       title='Successfully Purchased!'
       subTitle={`Order number: ${orderNumber}. Thank you for your purchase`}
       extra={[
-        <Button type='primary' key='console'>
+        <Button type='primary' key='console' onClick={navigatePage}>
           Buy Again
         </Button>,
       ]}
